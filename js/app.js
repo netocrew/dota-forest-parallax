b770fc49dfa147206c968640055e296c0a2bca23
netocrew <101523221+netocrew@users.noreply.github.com>
2023-06-12 19:08:15 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,3 +6,11 @@ document.addEventListener('mousemove', e => {
         `
     })
 })
+window.addEventListener('scroll', e => {
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+})
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content'
+})
